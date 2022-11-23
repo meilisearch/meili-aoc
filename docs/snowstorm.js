4ab1efcc75bacff03430f91b59bd224bf870be91
Guillaume Mourier <guillaume@meilisearch.com>
2022-11-23 01:16:08 +0100

    Remove flickering effect

--- docs/snowstorm.js
+++ docs/snowstorm.js
@@ -29,7 +29,7 @@ var snowStorm = (function(window, document) {
   this.snowStick = true;          // Whether or not snow should "stick" at the bottom. When off, will never collect.
   this.targetElement = null;      // element which snow will be appended to (null = document.body) - can be an element ID eg. 'myDiv', or a DOM node reference
   this.useMeltEffect = false;      // When recycling fallen snow (or rarely, when falling), have it "melt" and fade out if browser supports it
-  this.useTwinkleEffect = true;  // Allow snow to randomly "flicker" in and out of view while falling
+  this.useTwinkleEffect = false;  // Allow snow to randomly "flicker" in and out of view while falling
   this.usePositionFixed = false;  // true = snow does not shift vertically when scrolling. May increase CPU load, disabled by default - if enabled, used only where supported
   this.usePixelPosition = false;  // Whether to use pixel values for snow top/left vs. percentages. Auto-enabled if body is position:relative or targetElement is specified.
 
